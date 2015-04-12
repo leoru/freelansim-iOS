0c849d989bca8d325c851722a24b2be3b7edec94
Morozov Ivan <morozov@MacBook-Air-Morozov.local>
2015-04-12 18:18:18 +0500

    add UIImage category/ change separator

---
+++ freelansim-client/UIImage+RadialGradient.h
@@ -0,0 +1,13 @@
+//
+//  RTImageCategorie.h
+//  freelansim
+//
+//  Created by Morozov Ivan on 12.04.15.
+//  Copyright (c) 2015 Kirill Kunst. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RTImageCategorie : UIImage
+
+@end
---
+++ freelansim-client/UIImage+RadialGradient.m
@@ -0,0 +1,13 @@
+//
+//  RTImageCategorie.m
+//  freelansim
+//
+//  Created by Morozov Ivan on 12.04.15.
+//  Copyright (c) 2015 Kirill Kunst. All rights reserved.
+//
+
+#import "RTImageCategorie.h"
+
+@implementation RTImageCategorie
+
+@end
